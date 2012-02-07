eda2c032a7ae1423e9bf9bf72b67ef6b38189a52
Erick Fleming <erick@sudogs.com>
2012-02-07 14:42:02 -0600

    added lessengine

---
+++ test/LessEngineTest.js
@@ -0,0 +1,13 @@
+var buster     = require("buster"),
+    sinon      = require("sinon"),
+    Repository = require("../lib/lymph/Repository"),
+    LessEngine = require("../lib/lymph/LessEngine");
+
+buster.testCase("Less Engine", {
+
+    setUp: function(done) {
+        done();
+    }
+
+});
+
