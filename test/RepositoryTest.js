75e139bd2841e26c336f78266e2c73fdfd36d308
Erick Fleming <erick@sudogs.com>
2012-04-08 10:22:04 -0500

    integrated schema validator into repo

--- test/RepositoryTest.js
+++ test/RepositoryTest.js
@@ -1,6 +1,7 @@
 var buster     = require("buster"),
     Repository = require("../lib/lymph/Repository"),
-    Mongolian  = require("mongolian");
+    Mongolian  = require("mongolian"),
+    sinon      = require("sinon");
 
 buster.testCase("Repository", {
 
@@ -18,6 +19,7 @@ buster.testCase("Repository", {
 
         setUp: function(done){
             this.db = this.server.db("lymph");
+            this.validator = {validate:function(){}};
             done();
         },
 
@@ -29,16 +31,19 @@ buster.testCase("Repository", {
 
         "can insert objects into a collection": function(done){
             var self = this;
+            var mockValidator = sinon.mock(self.validator);
+            var objToBeInserted = {sampleId:"1", name:"foo"};
 
-            var repo = new Repository(self.db);
+            mockValidator.expects("validate").withArgs("sample", objToBeInserted).returns({isValid:true});
 
-            repo.insert("sample", "sampleId", {sampleId:"1", name:"foo"}, function(inserted){
+            var repo = new Repository(self.db, self.validator);
 
+            repo.insert("sample", "sampleId", objToBeInserted, function(inserted){
                 self.db.collection("sample").find().toArray(function(err, rst){
                     assert.equals(rst.length, 1);
+                    mockValidator.verify();
                     done();
                 });
-
             });
         },
 
@@ -73,15 +78,20 @@ buster.testCase("Repository", {
 
         "can update fields in an eixting object by id": function(done){
             var self = this;
+            var mockValidator = sinon.mock(self.validator);
+            var objToBeUpdated = {_id:"1", sampleId:"1", name:"foo"};
 
-            self.db.collection("sample").insert({_id:"1", sampleId:"1", name:"foo"}, function(err, inserted){
+            mockValidator.expects("validate").withArgs("sample", {name:"bar"}).returns({isValid:true});
 
-                var repo = new Repository(self.db);
+            self.db.collection("sample").insert(objToBeUpdated, function(err, inserted){
+
+                var repo = new Repository(self.db, self.validator);
 
                 repo.updateById("sample", "1", {name:"bar"}, function(){
 
                     self.db.collection("sample").find().toArray(function(err, items){
                         assert.equals(items[0], {_id:"1", sampleId:"1", name:"bar"});
+                        mockValidator.verify();
                         done();
                     });
                 });
@@ -95,15 +105,19 @@ buster.testCase("Repository", {
                 {_id:"2", sampleId:"2", name:"bar"},
             ];
 
+            var mockValidator = sinon.mock(self.validator);
+            mockValidator.expects("validate").withArgs("sample", {name:"foobar"}).returns({isValid:true});
+
             self.db.collection("sample").insert(items, function(err, inserted){
 
-                var repo = new Repository(self.db);
+                var repo = new Repository(self.db, self.validator);
 
                 repo.update("sample", {name:"foo"}, {name:"foobar"}, function(){
 
                     self.db.collection("sample").find().toArray(function(err, items){
                         assert.equals(items.length, 2);
                         assert.equals(items[1], {_id:"1", sampleId:"1", name:"foobar"});
+                        mockValidator.verify();
                         done();
                     });
                 });
